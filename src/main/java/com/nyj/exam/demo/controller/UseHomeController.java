4f9f7984b68a639d7c0965aa5701467ded1a1793
NamYeJi <dkdlelql123@naver.com>
2022-02-03 23:21:24 +0900

    재 접속시 카운트다운

--- src/main/java/com/nyj/exam/demo/controller/UseHomeController.java
+++ src/main/java/com/nyj/exam/demo/controller/UseHomeController.java
@@ -6,22 +6,26 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class UseHomeController {
+
+	private int num;
+	
+	public UseHomeController() {
+		num = 0;
+	}
+	
 	@RequestMapping("/usr/home/main")
 	@ResponseBody
-	public String showMain(){
-		return "hello!!";
+	public String showMain(){ 
+		return "hello";
 	}
 	
-	@RequestMapping("/usr/home/main2")
+	@RequestMapping("/usr/home/main4")
 	@ResponseBody
-	public String showMain2(){
-		return "hello!!2!";
-	}
-	
-	@RequestMapping("/usr/home/main3")
-	@ResponseBody
-	public String showMain3(){
-		return "hello!!2233333222!";
-	}
+	public int showCount(){
+		
+		num--;
+		
+		return num;
+	} 
 	
 }
