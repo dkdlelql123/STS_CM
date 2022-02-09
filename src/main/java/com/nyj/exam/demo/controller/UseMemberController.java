f9f162145340d6999ad58a2d510077ea9cb7aea5
NamYeJi <dkdlelql123@naver.com>
2022-02-09 12:25:37 +0900

    Ut.empty 함수 도입

--- src/main/java/com/nyj/exam/demo/controller/UseMemberController.java
+++ src/main/java/com/nyj/exam/demo/controller/UseMemberController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.nyj.exam.demo.service.ArticleService;
 import com.nyj.exam.demo.service.MemberService;
+import com.nyj.exam.demo.util.Util;
 import com.nyj.exam.demo.vo.Article;
 import com.nyj.exam.demo.vo.Member; 
 
@@ -20,17 +21,17 @@ public class UseMemberController {
 	@Autowired
 	private MemberService memberService;
 
-	//회원가입 시, 입력데이터 유효성 체크, 공백도 체크
+	//
 	@RequestMapping("/usr/member/doJoin")
 	@ResponseBody
 	public Object doJoin(String loginId, String loginPw, String email, String name, String nickname, String phoneNumber) {
 
-		if(loginId == null || loginId.trim().length() == 0) return "아이디를 입력해주세요.";
-		if(loginPw == null || loginPw.trim().length() == 0) return "비밀번호를 입력해주세요.";
-		if(email == null || email.trim().length() == 0) return "이메일을 입력해주세요.";
-		if(name == null || name.trim().length() == 0) return "이름을 입력해주세요.";
-		if(nickname == null || nickname.trim().length() == 0) return "닉네임을 입력해주세요.";
-		if(phoneNumber == null || phoneNumber.trim().length() == 0) return "전화번호를 입력해주세요.";
+		if(Util.empty(loginId)) return "아이디를 입력해주세요.";
+		if(Util.empty(loginPw)) return "비밀번호를 입력해주세요.";
+		if(Util.empty(email)) return "이메일을 입력해주세요.";
+		if(Util.empty(name)) return "이름을 입력해주세요.";
+		if(Util.empty(nickname)) return "닉네임을 입력해주세요.";
+		if(Util.empty(phoneNumber)) return "전화번호를 입력해주세요.";
 		
 		int memberId = memberService.doJoin(loginId, loginPw, email, name, nickname, phoneNumber);
 		
