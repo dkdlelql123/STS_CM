fe4b1a2b463a2c6e2783fc7232a0f1578a6f429e
NamYeJi <dkdlelql123@naver.com>
2022-02-18 13:45:51 +0900

    showArticleDetail에 ResultData 적용

--- src/main/java/com/nyj/exam/demo/controller/UseMemberController.java
+++ src/main/java/com/nyj/exam/demo/controller/UseMemberController.java
@@ -13,7 +13,8 @@ import com.nyj.exam.demo.service.ArticleService;
 import com.nyj.exam.demo.service.MemberService;
 import com.nyj.exam.demo.util.Util;
 import com.nyj.exam.demo.vo.Article;
-import com.nyj.exam.demo.vo.Member; 
+import com.nyj.exam.demo.vo.Member;
+import com.nyj.exam.demo.vo.ResultData; 
 
 @Controller
 public class UseMemberController { 
@@ -36,15 +37,15 @@ public class UseMemberController {
 		int memberId = memberService.doJoin(loginId, loginPw, email, name, nickname, phoneNumber);
 		
 		if(memberId == -1) {
-			return Util.f("중복되는 아이디(%s)가 있습니다.", loginId);
+			return ResultData.form("f-1", Util.f("중복되는 아이디(%s)가 있습니다.", loginId)); 
 		}
 		
 		if(memberId == -2) {
-			return Util.f("중복되는 이메일(%s)[%s]가 있습니다.", email, name);
+			return ResultData.form("f-2",  Util.f("중복되는 이메일(%s)[%s]가 있습니다.", email, name));
 		}
 		
 		Member member = memberService.getMemberById(memberId);
 		
-		return member;
+		return ResultData.form("s-1",  Util.f("%s님 회원가입이 완료되었습니다", loginId), member);
 	}	
 }
