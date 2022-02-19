7be2e684c23d5ecfe603c11c3f901a4f26242a0e
NamYeJi <dkdlelql123@naver.com>
2022-02-19 13:36:48 +0900

    modify, delect 에 resultData 적용

--- src/main/java/com/nyj/exam/demo/controller/UseArticleController.java
+++ src/main/java/com/nyj/exam/demo/controller/UseArticleController.java
@@ -16,16 +16,6 @@ import com.nyj.exam.demo.vo.ResultData;
 
 @Controller
 public class UseArticleController {
-	
-//	M
-//	서비스, 레포지토리, db, data
-// ☆순서 : 컨트롤러 > 서비스 > dao > DB
-	
-//	V
-//	jsp(, js, css, html)
-	
-//	C
-//	컨트롤러 
 
 	@Autowired
 	private ArticleService articleService; 
@@ -39,7 +29,7 @@ public class UseArticleController {
 		
 		Article article = articleService.getArticle(id);
 		
-		return ResultData.form("s-1", Util.f("%s번째 글이 작성되었습니다",id), article);
+		return ResultData.form("s-1", Util.f("%s번째 글이 작성되었습니다",id), article);	
 	}
 	
 	@RequestMapping("/usr/article")
@@ -50,31 +40,34 @@ public class UseArticleController {
 			return ResultData.form("f-1",  Util.f("%s번째 게시글이 없습니다.", id));
 		}
 		
-		return ResultData.form("s-1",  Util.f("%s번째 게시글 입니다.", id), article);
+		return ResultData.form("s-1",  "게시판을 찾았습니다.", article);
+		
 	}
 	
 	@RequestMapping("/usr/article/delete")
 	@ResponseBody
-	public String doDelete(int id){
+	public ResultData doDelete(int id){
 		Article article = articleService.getArticle(id);
 		if( article == null ) {
-			return "게시물이 없습니다";
+			return ResultData.form("f-1", "게시물이 없습니다");
 		}
 		
 		articleService.deleteArticle(id);
-		return id+"번 게시물이 삭제완료";
+		
+		return ResultData.form("s-1", Util.f("%s 게시물이 삭제되었습니다", id));
 	}
 	
 	@RequestMapping("/usr/article/modify")
 	@ResponseBody
-	public String doModify(int id, String title, String body){
+	public ResultData doModify(int id, String title, String body){
 		Article article = articleService.getArticle(id);
 		if( article == null ) {
-			return "게시물이 없습니다";
+			return ResultData.form("f-1", "게시물이 없습니다");
 		}
 		
 		articleService.modifyArticle(id, title, body);
-		return id+"번 게시물이 수정완료";
+
+		return ResultData.form("s-1", Util.f("%s 게시물이 수정되었습니다", id)); 
 	}
  
 	
